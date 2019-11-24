2fda40ccef48544c95f8e36991724e710829a355
xiningchen <xiaoxining@hotmail.com>
2019-11-23 21:02:34 -0700

    removed redundant imports in DatabaseHelper class

--- app/src/main/java/com/example/spoonacular/DatabaseHelper.java
+++ app/src/main/java/com/example/spoonacular/DatabaseHelper.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.net.wifi.p2p.WifiP2pManager;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
